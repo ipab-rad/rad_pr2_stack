1dd2da765d3f780398e943d52b4e64a75a91a2c2
Emmanuel Kahembwe <ekahembwe@gmail.com>
2016-05-02 23:06:22 +0100

    Using the python moveit moveit_commander for the PR2. Not yet tested on the real PR2.

---
+++ pr2_demo/src/pr2_py/src/pr2_demo.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+import sys
+import copy
+import rospy
+import moveit_commander
+import moveit_msgs.msg
+import geometry_msgs.msg
+
+from std_msgs.msg import String
+
+def pick_n_place():
+    ## Inititialise moveit_commander and rospy
+    print "========Inititialise moveit_commander and rospy"
+    raw_input("Press Enter to continue...")
+    moveit_commander.roscpp_initialize(sys.argv)
+    rospy.init_node('pick_n_place', anonymous=True)
+
+    robot = moveit_commander.RobotCommander()
+    scene = moveit_commander.PlanningSceneInterface()
+
+    left_arm = moveit_commander.MoveGroupCommander("left_arm");
+    right_arm = moveit_commander.MoveGroupCommander("right_arm");
+
+
+    display_trajectory_publisher = rospy.Publisher(
+        '/move_group/display_planned_path',
+        moveit_msgs.msg.DisplayTrajectory)
+
+    # Wait for RVIZ
+    #rospy.sleep(10)
+
+    print "============ Generating plan"
+    raw_input("Press Enter to continue if Rviz visable...")
+    pose_target = geometry_msgs.msg.Pose()
+    pose_target.orientation.w = 1.0
+    pose_target.position.x = 0.7
+    pose_target.position.y = 0.05
+    pose_target.position.z = 1.0
+    left_arm.set_pose_target(pose_target)
+
+    plan1 = left_arm.plan()
+
+    print "============ Waiting while RVIZ displays plan1..."
+    rospy.sleep(5)
+
+    # Uncomment below line when working with a real robot
+    #group.go(wait=True)
+    #rospy.sleep(10)
+    #raw_input("Press Enter to continue...")
+
+    raw_input("Motion Completed\nPress Enter to Shutdown..")
+
+
+
+
+if __name__=='__main__':
+    try:
+        pick_n_place()
+    except rospy.ROSInterruptException:
+        pass
+
